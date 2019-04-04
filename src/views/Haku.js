fd922c9dddc30153949e5aa6576fe6ef83e4cf16
Aleksanteri Heliövaara <aleksanteriheliovaara@aleksanterismbp.lan>
2019-04-04 16:42:10 +0300

    first commit

--- src/views/Haku.js
+++ src/views/Haku.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import Nav from 'components/Nav';
 import Container from 'components/Container';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Locations from 'components/Map';
 import { Button } from 'components/Button';
+import { Row, Col } from 'react-flexbox-grid';
 
+const Input = styled.input`
+		padding-top: 10px;
+		padding-bottom: 10px; 
+		min-width: 100%;
+		font-size: 20px;
+		color: ${props => props.theme.colors.black};
+		padding-left: 20px;
+`;
+const Form = styled.form`
+	margin-top: 20px;
+	margin-bottom: 20px;
+`;
+
+const SearchButton = styled(Button)`
+	min-width: 100%
+`
 const Haku = ({ history }) => {
 	const initialAddress = "Maneesikatu";
 	const [address, setAddress] = useState(initialAddress);
@@ -53,13 +71,20 @@ const Haku = ({ history }) => {
 	
 	return (
 		<div>
-		<Nav transparent/>  
-			<Header >
-				<form action="" onSubmit={handleForm}>
-					<input placeholder="Hae" type="text"  onChange={ e => setAddress(e.target.value) }/>
-					<Button  type="submit" primary>Hae</Button>
-				</form>
-			</Header>
+			<Nav transparent/>  
+				<Container>
+					<Form action="" onSubmit={handleForm}>
+						<Row>
+							<Col xs={6} sm={6} md={6} lg={6} >
+								<Input placeholder="Hae" type="text"  onChange={ e => setAddress(e.target.value) }/>
+							</Col>
+							<Col smOffset={2} mdOffset={2} lgOffset={2} xsOffset={2} xs={4} sm={4} md={4} lg={4} >
+								<SearchButton type="submit" primary>Hae</SearchButton>
+							</Col>
+						</Row>
+					</Form>
+				</Container>
+				
 			<Container>
 				<Locations initialLat={lat} initialLon={lon} history={history}/>
 			</Container>
